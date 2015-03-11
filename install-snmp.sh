f92f7c1d57f7167ad447dba985dde40d5b7ea5d2
Dennis Chen <barracks510@gmail.com>
2015-03-10 22:46:10 -0700

    add install shell

---
+++ install-snmp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp ./snmp_persist_aquaero5.pl /usr/local/sbin
+chmod +x /usr/local/sbin/snmp_persist_aquaero5.pl
+
+echo "pass_persist .1.3.6.1.4.1.2021.255.65.67 /usr/local/sbin/snmp_persist_aquaero5.pl" >> /etc/snmp/snmpd.conf
+
